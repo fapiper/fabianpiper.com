4ed3cbbb7c917ec783d29847b83ad4255810d637
Fabian Piper <fabianpiper@web.de>
2026-01-28 14:39:40 +0100

    fix: data util errors

--- www/src/lib/data-utils.ts
+++ www/src/lib/data-utils.ts
@@ -22,12 +22,14 @@ export async function getAllPostsAndSubposts(): Promise<
 }
 
 export async function getAllProjects(): Promise<CollectionEntry<'projects'>[]> {
-  const projects = await getCollection('projects')
+  return getCollection('projects')
+  /*
   return projects.sort((a, b) => {
-    const dateA = a.data.startDate?.getTime() || 0
-    const dateB = b.data.startDate?.getTime() || 0
+    const dateA = a.data.date?.getTime() || 0
+    const dateB = b.data.date?.getTime() || 0
     return dateB - dateA
   })
+*/
 }
 
 export async function getAllPublications(): Promise<
